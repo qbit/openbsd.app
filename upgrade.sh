41420cd121d477a6d0b3eb2a18e4b92c72e75188
Aaron Bieber <aaron@bolddaemon.com>
2023-08-17 09:06:15 -0600

    use a var for sqlports version, update to latest

--- upgrade.sh
+++ upgrade.sh
@@ -4,6 +4,8 @@ set -xe
 
 mkdir -p /tmp/openbsd_app/{stable,current}
 
+CURRENT_VER="7.40"
+STABLE_VER="7.37"
 CURRENT_FILE=${1:-/tmp/openbsd_app/current/share/sqlports}
 STABLE_FILE=${2:-/tmp/openbsd_app/stable/share/sqlports}
 SIGNIFY="${SIGNIFY:-signify}"
@@ -12,18 +14,18 @@ STABLE_PUB=$(readlink -f /etc/signify/openbsd-73-pkg.pub)
 
 (
 	cd /tmp/openbsd_app/current
-	curl -L -O https://cdn.openbsd.org/pub/OpenBSD/snapshots/packages/amd64/sqlports-7.38.tgz
+	curl -L -O https://cdn.openbsd.org/pub/OpenBSD/snapshots/packages/amd64/sqlports-${CURRENT_VER}.tgz
 	curl -L -O https://cdn.openbsd.org/pub/OpenBSD/snapshots/packages/amd64/SHA256.sig
-	${SIGNIFY} -C -p ${CURRENT_PUB} -x SHA256.sig sqlports-7.38.tgz
-	tar -C . -zxvf sqlports-7.38.tgz
+	${SIGNIFY} -C -p ${CURRENT_PUB} -x SHA256.sig sqlports-${CURRENT_VER}.tgz
+	tar -C . -zxvf sqlports-${CURRENT_VER}.tgz
 )
 
 (
 	cd /tmp/openbsd_app/stable
-	curl -L -O https://cdn.openbsd.org/pub/OpenBSD/7.3/packages/amd64/sqlports-7.37.tgz
+	curl -L -O https://cdn.openbsd.org/pub/OpenBSD/7.3/packages/amd64/sqlports-${STABLE_VER}.tgz
 	curl -L -O https://cdn.openbsd.org/pub/OpenBSD/7.3/packages/amd64/SHA256.sig
-	${SIGNIFY} -C -p ${STABLE_PUB} -x SHA256.sig sqlports-7.37.tgz
-	tar -C . -zxvf sqlports-7.37.tgz
+	${SIGNIFY} -C -p ${STABLE_PUB} -x SHA256.sig sqlports-${STABLE_VER}.tgz
+	tar -C . -zxvf sqlports-${STABLE_VER}.tgz
 )
 
 SQL=$(cat <<EOF
